7f5443998d9df8b2be350ce5a3b65e4081852ef0
Vishakha02D2 <120273836+Vishakha02D2@users.noreply.github.com>
2023-03-28 00:03:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 //your JS code here. If required.
-onst panels = document.querySelectorAll('.panel')
- 
- 
-panels.forEach(panel=>{
-    panel.addEventListener("click",function(){
-        removeActiveClasses()
-        panel.classList.add("active")
-    })
-})
- 
-function removeActiveClasses(){
-    panels.forEach(panel=>{
-        panel.classList.remove("active")
-    })
-}
